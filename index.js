33c6cb4a3b7803dce4e23a877d40395cc6088d31
Sanket <sanketanilkadam@gmail.com>
2023-01-15 15:13:33 +0530

    added custom replies

--- index.js
+++ index.js
@@ -1,36 +1,44 @@
 const { Configuration, OpenAIApi } = require("openai");
 require("dotenv").config();
 const auth = require("./auth");
+const responses = require("./replies");
 
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
 });
 const openai = new OpenAIApi(configuration);
 
-auth().then((client) =>
-  client.on("message", async (msg) => {
-    isMention = msg.body.includes(`@${client.info.me.user}`);
-    if (msg.body == `@${client.info.me.user}`) {
-      msg.reply(`How can I help you ${msg._data.notifyName} ?`);
-    } else {
-      if (msg.body.startsWith("#") || isMention) {
-        const chat = await msg.getChat();
-        const prompt = isMention
-          ? msg.body.substring(13)
-          : msg.body.substring(1);
-        try {
-          chat.sendStateTyping();
-          gptResponse(prompt).then((result) => {
-            msg.reply(result);
-            chat.clearState();
-          });
-        } catch (error) {
-          console.log(error);
+try {
+  auth().then((client) =>
+    client.on("message", async (msg) => {
+      isMention = msg.body.includes(`@${client.info.me.user}`);
+      if (msg.body == `@${client.info.me.user}`) {
+        msg.reply(`How can I help you ${msg._data.notifyName} ?`);
+      } else {
+        if (msg.body.startsWith("#") || isMention) {
+          let index = responses.findIndex((response) =>
+            msg.body.toLowerCase().includes(response.que.toLowerCase())
+          );
+          if (index >= 0) {
+            msg.reply(responses[index]?.ans);
+          } else {
+            const chat = await msg.getChat();
+            const prompt = isMention
+              ? msg.body.substring(13)
+              : msg.body.substring(1);
+            chat.sendStateTyping();
+            gptResponse(prompt).then((result) => {
+              msg.reply(result);
+              chat.clearState();
+            });
+          }
         }
       }
-    }
-  })
-);
+    })
+  );
+} catch (error) {
+  console.log(error);
+}
 
 async function gptResponse(message) {
   try {
---
+++ replies.js
@@ -0,0 +1,12 @@
+const responses = [
+  { que: "what is your name", ans: "My name is Sanket " },
+  { que: "who are you", ans: "I'm a AI chatbot Developed by Sanket" },
+  {
+    que: "tell me about yourself",
+    ans: "I'm a AI chatbot Developed by Sanket",
+  },
+  { que: "what's your name", ans: "My name is Sanket " },
+  { que: "tell me your name", ans: "My name is Sanket " },
+];
+
+module.exports = responses;
