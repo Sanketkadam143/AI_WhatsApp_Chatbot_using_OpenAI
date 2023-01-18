91a5a683c7ce6bf6e7fec815f5da424532e80448
Sanket <sanketanilkadam@gmail.com>
2023-01-18 16:19:52 +0530

    added error boundaries

--- index.js
+++ index.js
@@ -8,37 +8,49 @@ const configuration = new Configuration({
 });
 const openai = new OpenAIApi(configuration);
 
-try {
-  auth().then((client) =>
+auth()
+  .then((client) =>
     client.on("message", async (msg) => {
       isMention = msg.body.includes(`@${client.info.me.user}`);
       if (msg.body == `@${client.info.me.user}`) {
-        msg.reply(`How can I help you ${msg._data.notifyName} ?`);
+        try {
+          msg.reply(`How can I help you ${msg._data.notifyName} ?`);
+        } catch (error) {
+          console.log(error);
+        }
       } else {
         if (msg.body.startsWith("#") || isMention) {
           let index = responses.findIndex((response) =>
             msg.body.toLowerCase().includes(response.que.toLowerCase())
           );
           if (index >= 0) {
-            msg.reply(responses[index]?.ans);
+            try {
+              msg.reply(responses[index]?.ans);
+            } catch (error) {
+              console.log(error);
+            }
           } else {
-            const chat = await msg.getChat();
-            const prompt = isMention
-              ? msg.body.substring(13)
-              : msg.body.substring(1);
-            chat.sendStateTyping();
-            gptResponse(prompt).then((result) => {
-              msg.reply(result);
-              chat.clearState();
-            });
+            try {
+              const chat = await msg.getChat();
+              const prompt = isMention
+                ? msg.body.substring(13)
+                : msg.body.substring(1);
+              chat.sendStateTyping();
+              gptResponse(prompt)
+                .then((result) => {
+                  msg.reply(result);
+                  chat.clearState();
+                })
+                .catch((err) => console.log(err));
+            } catch (error) {
+              console.log(error);
+            }
           }
         }
       }
     })
-  );
-} catch (error) {
-  console.log(error);
-}
+  )
+  .catch((error) => console.log(error));
 
 async function gptResponse(message) {
   try {
